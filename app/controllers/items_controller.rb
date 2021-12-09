9be215a6868e94e85258b2121537ce6da8a1729d
sovetbek <akmatkulovs991@gmail.com>
2021-12-09 23:43:56 +0300

    Add file new.html.erb & edit.html.erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,18 +3,34 @@ class ItemsController < ApplicationController
   layout false
   skip_before_action :verify_authenticity_token
 
-  def index
+  def index # Показывает все что есть в базе данных
     @items = Item.all
       render body: @items.map { |i| "#{i.name}: #{i.price}"}
   end
 
-  def create
+  def create # Создает обьект в базу
     item = Item.create(items_params)
     if item.persisted?
       render json: item.name, status: :created
     else
       render json: item.errors, status: :unprocessable_entity
     end
+  end
+
+  def new # Рендерит форму и посылается методу create
+   # создаем новый объект модели item и присваиваем его переменной экземпляра
+  end
+
+  def show # Показыает item по текущему id
+  end
+
+  def edit # При вызове метода edit он будет посылатся на метод update
+  end
+
+  def update # Обновляет обьект в базе данных
+  end
+
+  def destroy # Удаляет обьект с базы данных
   end
 
   private
